3cc053ee5de035ee16843e47fdbfad1980578f9c
AdeyemiA <aoa2124@columbia.edu>
2014-12-05 19:25:50 -0800

    Created 2 controller topology

--- mininet/testTopo.py
+++ mininet/testTopo.py
@@ -24,45 +24,50 @@ def multiControllerNet():
     print "*** Creating (reference) controllers"
     
     c1 = RemoteController( 'c1', port=6633 )
-    #c2 = RemoteController( 'c2', port=6644 )
+    c2 = RemoteController( 'c2', port=6644 )
 
 
     #c1 = net.addController('c1')
     #c2 = net.addController('c2')
     
     print "*** Creating switches"
-    #s1 = net.addSwitch( 's1' )
-    #s2 = net.addSwitch( 's2' )
+    s1 = net.addSwitch( 's1' )
+    s2 = net.addSwitch( 's2' )
     s3 = net.addSwitch( 's3' ) 
     s4 = net.addSwitch( 's4' )
     print "*** Creating hosts"
     
-    #hosts1 = [ net.addHost( 'h%d' % n ) for n in 1, 2 ]
+    hosts1 = [ net.addHost( 'h%d' % n ) for n in 1, 2 ]
     #hosts2 = [ net.addHost( 'h%d' % n ) for n in 3, 4 ]
-    hosts3 = [ net.addHost( 'h%d' % n ) for n in 5, 6 ]
-    hosts4 = [ net.addHost( 'h%d' % n ) for n in 7, 8 ]
+    #hosts3 = [ net.addHost( 'h%d' % n ) for n in 5, 6 ]
+    hosts2 = [ net.addHost('h3') ]
+    hosts3 = [ net.addHost('h4') ]
+    hosts4 = [ net.addHost( 'h%d' % n ) for n in 5, 6 ]
     print "*** Creating links"
-    #for h in hosts1:
-     #   net.addLink( s1, h )
-    #for h in hosts2:
-     #   net.addLink( s2, h )
+    for h in hosts1:
+        net.addLink( s1, h )
+    for h in hosts2:
+        net.addLink( s2, h )
     for h in hosts3:
         net.addLink( s3, h )
     for h in hosts4:
         net.addLink( s4, h )
 
-    #net.addLink( s1, s2 )
-    #net.addLink( s2, s3 )
+    net.addLink( s1, s2 )
+    net.addLink( s2, s3 )
     net.addLink( s3, s4 )
+    #net.addLink( s2, h3 )
+    #net.addLink( s3, h4 )
+
 
     print "*** Starting network"
     net.build()
     c1.start()
-   # c2.start()
-   # s1.start( [ c1 ] )
-   # s2.start( [ c1 ] )
-    s3.start( [ c1 ] ) 
-    s4.start( [ c1 ] ) 
+    c2.start()
+    s1.start( [ c1 ] )
+    s2.start( [ c2 ] )
+    s3.start( [ c2 ] ) 
+    s4.start( [ c2 ] ) 
 
     print "*** Testing network"
     # net.pingAll()
